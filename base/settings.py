f9f7a849a9e85211a6822751d77872abdcc61047
conlon0121@gmail.com <conlon0121@gmail.com>
2017-06-04 19:46:12 -0400

    added default authentication to settings

--- base/settings.py
+++ base/settings.py
@@ -63,8 +63,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
     ]
 }
 
