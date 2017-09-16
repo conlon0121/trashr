12ca80e9c33fe1fa6b9e2717855ecc07ac9bad6f
conlon0121@gmail.com <conlon0121@gmail.com>
2017-09-16 19:13:16 -0400

    changed ci file

--- base/settings.py
+++ base/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'trashr',
         'USER': os.environ.get('user'),
-        'PASSWORD': os.environ.get('pass'),
+        'PASSWORD': os.environ.get('DATABAS_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
         'PORT': '',
     }
