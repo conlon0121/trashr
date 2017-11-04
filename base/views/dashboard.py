97de5894473d0c951fccb8982c7e559001eafa8a
conlon0121@gmail.com <conlon0121@gmail.com>
2017-11-04 00:00:33 -0400

    idk what ive done

--- base/views/dashboard.py
+++ base/views/dashboard.py
@@ -15,8 +15,14 @@ class DashboardView(View):
     def get(self, request):
         dumpsters = Dumpster.objects.all()
         features = []
+        lat = 0
+        long = 0
         for dumpster in dumpsters:
             fill = dumpster.percent_fill
+            d_lat = float(dumpster.latitude)
+            d_long = float(dumpster.longitude)
+            lat += d_lat
+            long += d_long
             if fill < 30:
                 color = "#00a600"
             elif fill > 50:
@@ -26,19 +32,29 @@ class DashboardView(View):
             features.append({
                 "type": "Feature",
                 "properties": {
-                    "description": str(dumpster.percent_fill),
+                    "description": f'{dumpster.address}<br/>'
+                                   f'<center>{str(dumpster.percent_fill)}% full</center>'
+                                   f'<button id={str(dumpster.id)}>select in table</button>',
                     "marker-color": color,
-                    "marker-size": "small"
+                    "marker-size": "small",
+                    "marker-symbol": f'{str(dumpster.percent_fill)}'
                 },
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [float(dumpster.longitude), float(dumpster.latitude)]
+                    "coordinates": [d_long, d_lat]
                 }
             })
+        try:
+            lat = lat / len(features)
+            long = long / len(features)
+        except ZeroDivisionError:
+            pass
         table = DumpsterTable(dumpsters)
         form_filter = self.form_class_filter()
         form_select = self.form_class_select()
         return render(request, self.template_name, {'table': table,
                                                     'form_filter': form_filter,
                                                     'form_select': form_select,
+                                                    'lat': lat,
+                                                    'long': long,
                                                     'layer': json.dumps(features)})
