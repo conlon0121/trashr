d7a9501285bd0d7f30c143e11b3dac15c79d7b99
conlon0121@gmail.com <conlon0121@gmail.com>
2018-01-02 14:30:07 -0500

    removed whitenoise from settings

--- base/settings.py
+++ base/settings.py
@@ -168,7 +168,6 @@ SASS_PROCESSOR_AUTO_INCLUDE = False
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SASS_PROCESSOR_INCLUDE_DIRS = [
     os.path.join(STATIC_ROOT, 'sass'),
